509eb414bcf18ea705ee84f71cfee6c5f86d8ed5
Muxammadali <sherikjumayev1202@gmail.com>
2023-01-08 23:49:48 +0500

    Task_30 - Seminar_4

---
+++ Semunar_4/Task_30/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] result = CreateArrayEightElements();
+PrintArray(result);
+
+int[] CreateArrayEightElements()
+{
+    Random random = new Random();
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 2);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
