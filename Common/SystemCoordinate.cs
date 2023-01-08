48413029c3a5523e1dcf0c63740ec8d80e6eea97
Muxammadali <sherikjumayev1202@gmail.com>
2023-01-08 14:25:02 +0500

    Till the Seminar_4

---
+++ Common/SystemCoordinate.cs
@@ -0,0 +1,44 @@
+﻿namespace Common;
+
+    public static class SystemCoordinate
+{
+<<<<<<< HEAD
+
+    public static int GetNumberFromUser()
+    {
+        bool Parsed = int.TryParse(Console.ReadLine(), out int number);
+        
+        if(Parsed)
+        {
+            return number;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+=======
+>>>>>>> main
+    public static int GetNumberOfQuarter(int x, int y)
+    {
+        if (x > 0 && y > 0)
+        {
+            return 1;
+        }
+
+        if (x < 0 && y > 0)
+        {
+            return 2;
+        }
+        if (x < 0 && y < 0)
+        {
+            return 3;
+        }
+
+        if (x > 0 && y < 0)
+        {
+            return 4;
+        }
+        return -1;
+    }
+}
