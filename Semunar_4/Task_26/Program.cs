b6cc70cae8d0ee1d9ace90173903ad444929a716
Muxammadali <sherikjumayev1202@gmail.com>
2023-01-08 15:09:43 +0500

    Task_26 - Seminar_4

--- Semunar_4/Task_26/Program.cs
+++ Semunar_4/Task_26/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3 78 -> 2 89126 -> 5
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetNumberLength(number));
+
+int GetNumberLength(int number)
+{
+    int length = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        length++;
+    }
+    return length;
+}
+
+
+
+
+
+
+
+
+
+
+
+
